8c78d460815dadf8bf5bba290d80f749cf62c3bb
Olaleye Igbagbo <89357459+igee-17@users.noreply.github.com>
2022-10-21 09:00:58 +0100

    Update get_flag.c

--- get_flag.c
+++ get_flag.c
@@ -2,11 +2,11 @@
 
 /**
  * get_flag - turns on flags if _printf finds
- * a flag modifier in the format string
- * @s: character that holds the flag specifier
+ * a flag modifier in the format string format
+ * @s: character that holds the flag specifier in code base
  * @f: pointer to the struct flags in which we turn the flags on
  *
- * Return: 1 if a flag has been turned on, 0 otherwise
+ * Return: 1 if a flag has been turned on, 0 otherwise would be returned
  */
 int get_flag(char s, flags_t *f)
 {
