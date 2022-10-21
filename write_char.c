1e676e6d68adb18cedf8d90ad5a18c51a7498273
Olaleye Igbagbo <89357459+igee-17@users.noreply.github.com>
2022-10-21 09:29:03 +0100

    Update write_char.c

--- write_char.c
+++ write_char.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: On success 1.
+* _putchar - writes the character c to stdout on memory call
+* @c: The character to print in ASCII
+* Return: On success 1. null on zero
 * On error, -1 is returned, and errno is set appropriately.
 * Description: _putchar uses a local buffer of 1024 to call write
-* as little as possible
+* as little as possible to memory
 */
 int _putchar(char c)
 {
@@ -28,9 +28,9 @@ return (1);
 }
 
 /**
-* _puts - prints a string to stdout
-* @str: pointer to the string to print
-* Return: number of chars written
+* _puts - prints a string to stdout in 64bit
+* @str: pointer to the string to print hello world
+* Return: number of chars written in ASCII
 */
 int _puts(char *str)
 {
