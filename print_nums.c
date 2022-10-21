be11985a2df1dc1ef3423cd80141bda6c34bfa05
Olaleye Igbagbo <89357459+igee-17@users.noreply.github.com>
2022-10-21 09:26:14 +0100

    Update print_nums.c

--- print_nums.c
+++ print_nums.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_int - prints an integer
- * @l: va_list of arguments from _printf
+ * print_int - prints an integer value
+ * @l: va_list of arguments from _printf on both 32bit and 64bit machine
  * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
+ * if a flag is passed to _printf and if not returns null
+ * Return: number of char printed in integer
  */
 int print_int(va_list l, flags_t *f)
 {
@@ -23,11 +23,11 @@ int print_int(va_list l, flags_t *f)
 }
 
 /**
- * print_unsigned - prints an unsigned integer
+ * print_unsigned - prints an unsigned integer with pointer to mem alloc
  * @l: va_list of arguments from _printf
  * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
+ * if a flag is passed to _printf or not
+ * Return: number of char printed as ASCII
  */
 int print_unsigned(va_list l, flags_t *f)
 {
@@ -40,8 +40,8 @@ int print_unsigned(va_list l, flags_t *f)
 
 /**
  * print_number - helper function that loops through
- * an integer and prints all its digits
- * @n: integer to be printed
+ * an integer and prints all its digits in real time
+ * @n: integer to be printed as a value
  */
 void print_number(int n)
 {
@@ -63,8 +63,8 @@ void print_number(int n)
 /**
  * count_digit - returns the number of digits in an integer
  * for _printf
- * @i: integer to evaluate
- * Return: number of digits
+ * @i: integer to evaluate to zero or one
+ * Return: number of digits returned
  */
 int count_digit(int i)
 {
